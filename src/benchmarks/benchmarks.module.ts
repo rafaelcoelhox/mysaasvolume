1e657658894b6f6326aefcd87bae59e958a1761b
rafael <coelho38r@proton.me>
2026-01-11 19:50:13 -0300

    deleted

--- src/benchmarks/benchmarks.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BenchmarksService } from './benchmarks.service';
-
-@Module({
-  providers: [BenchmarksService],
-  exports: [BenchmarksService],
-})
-export class BenchmarksModule {}
